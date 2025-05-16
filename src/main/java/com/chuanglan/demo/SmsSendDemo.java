7a2bee11993651ba33bb0ea864066b32734984af
jakoes.wu <jakoes.wu@anyifa.cn>
2025-05-16 10:33:23 +0800

    init

--- src/main/java/com/chuanglan/demo/SmsSendDemo.java
+++ src/main/java/com/chuanglan/demo/SmsSendDemo.java
@@ -16,17 +16,17 @@ import com.chuanglan.util.ChuangLanSmsUtil;
 public class SmsSendDemo {
     public static final String charset = "utf-8";
     // 用户平台API账号(非登录账号,示例:N1234567)
-    public static String account = "";
+    public static String account = "N8263717";
     // 用户平台API密码(非登录密码)
-    public static String password = "";
+    public static String password = "U27vundKms4853";
 
     public static void main(String[] args) throws UnsupportedEncodingException {
         //请求地址请登录253云通讯自助通平台查看或者询问您的商务负责人获取
-        String smsSingleRequestServerUrl = "https://xxx/msg/send/json";
+        String smsSingleRequestServerUrl = "https://smssh1.253.com/msg/v1/send/json";
         // 短信内容
-        String msg = "【253云通讯】你好,你的验证码是123456";
+        String msg = "【安易发】验证码为876922，有效时间为5分钟，请及时验证。";
         //手机号码
-        String phone = "159********";
+        String phone = "13917302719";
         //状态报告
         String report= "true";
 
